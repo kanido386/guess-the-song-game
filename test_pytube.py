c0cde21cbe42a4262aeb1052867213e8a2d82866
kanido386 <kanido386@gmail.com>
2021-03-22 01:25:58 +0800

    Try out package: pytube

---
+++ test_pytube.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+# https://github.com/pytube/pytube
+from pytube import YouTube
+
+import os
+import time
+import librosa
+from scipy.io.wavfile import write
+
+# Turn off the warning "PySoundFile failed. Trying audioread instead."
+import warnings
+warnings.filterwarnings('ignore')
+
+print('==============================')
+print('     輸入「stop」來結束程式')
+print('==============================')
+
+while True:
+
+  filepath = './Audio'
+  video_url = input('輸入影片網址: ')
+  if video_url == 'stop':
+    break
+  song_name = input('輸入歌名: ')
+  if song_name == 'stop':
+    break
+
+  time.sleep(1)
+
+  print('（下載中）')
+
+  if not os.path.exists(filepath):
+    os.mkdir(filepath)
+
+  YouTube(video_url) \
+    .streams \
+    .filter(adaptive=True, only_audio=True) \
+    .order_by('abr') \
+    .last() \
+    .download(output_path=filepath, filename='temp')
+
+  print('（轉檔中）')
+
+  y, sr = librosa.load(f'{filepath}/temp.webm')
+
+  if os.path.exists(f'{filepath}/temp.webm'):
+    os.remove(f'{filepath}/temp.webm')
+  else:
+    print('The file does not exist')
+
+  write(f'{filepath}/{song_name}.wav', sr, y)
+
+  print('（完成）')
+  
+  time.sleep(1)
+  
+  print('檔案路徑為:', os.path.abspath(f'{filepath}/{song_name}.wav'))
+  print('==============================')
+
+  time.sleep(3)
