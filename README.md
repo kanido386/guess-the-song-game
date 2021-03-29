8521c8d6fb6b9597b56f8ac8559d09564e9847dd
kanido386 <kanido386@gmail.com>
2021-03-29 17:07:50 +0800

    Try out youtube data api

--- README.md
+++ README.md
@@ -4,9 +4,9 @@
 [三個麻瓜](https://www.youtube.com/channel/UC-Es7ozDeMMPy9_jH6uL5TA) - [另類猜歌九宮格](https://www.youtube.com/watch?v=hhlk5GABq64)
 
 ### 實作相關
-- [ ] 用 YouTube Data API 抓 YouTube 影片的網址
+- [x] 用 YouTube Data API 抓 YouTube 影片的網址
 - [x] 用「YouTube 轉 mp3」的相關套件抓歌曲音檔
-- [ ] 想抓「某某」歌 => 用「某某 歌詞」當關鍵詞搜尋 YouTube => 找觀看次數最高且 title 有「歌詞」的影片當作目標 => 請套件做他該做的事情
+- [x] 想抓「某某」歌 => 用「某某 lyrics」當關鍵詞搜尋 YouTube => 找最相關的影片當作目標 => 請套件做他該做的事情
 - [ ] 用 API 或爬蟲抓取 KKBOX 排行榜的歌名
 
 ### 遊戲模式
---
+++ test_youtube_data_api.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+# https://developers.google.com/youtube/v3/docs/search/list
+from googleapiclient.discovery import build
+
+import config
+
+youTubeApiKey = config.api_key['youtube']
+youtube = build('youtube', 'v3', developerKey=youTubeApiKey)
+
+print('==============================')
+print('     輸入「stop」來結束程式')
+print('==============================')
+
+while True:
+  query = input('請輸入歌曲名稱: ')
+  if query == 'stop':
+    break
+  
+  request = youtube.search().list(
+    part='snippet',
+    q=f'{query} lyrics',
+    type='video'
+  )
+  response = request.execute()
+
+  # # Print video url & title of the five most relevant videos
+  # for item in response['items']:
+  #   videoId = item['id']['videoId']
+  #   print(f'https://www.youtube.com/watch?v={videoId}')
+  #   print(item['snippet']['title'])
+  #   print('-----')
+
+  # The video that is most relevant to the search query
+  firstVideo = response['items'][0]
+
+  videoId = firstVideo['id']['videoId']
+  videoUrl = f'https://www.youtube.com/watch?v={videoId}'
+  print('影片網址:', videoUrl)
+  print('==============================')
