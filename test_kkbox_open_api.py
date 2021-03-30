d01c30ff66bbc3c346883d7ef4097ad67c39cf77
kanido386 <kanido386@gmail.com>
2021-03-30 15:03:22 +0800

    Can get list of tracks of a chart playlist

--- test_kkbox_open_api.py
+++ test_kkbox_open_api.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
+# https://www.learncodewithmike.com/2020/02/python-kkbox-open-api.html
+
 import requests
 import credentials
 
-
-# https://www.learncodewithmike.com/2020/02/python-kkbox-open-api.html
 def get_access_token():
   
   url = 'https://account.kkbox.com/oauth2/token'
@@ -24,5 +24,76 @@ def get_access_token():
   return access_token.json()['access_token']
 
 
+# https://docs-en.kkbox.codes/#get-/charts
+def get_chart_playlists():
+  
+  access_token = get_access_token()
 
-print(get_access_token())
+  url = 'https://api.kkbox.com/v1.1/charts'
+
+  headers = {
+    'accept': 'application/json',
+    'authorization': f'Bearer {access_token}'
+  }
+
+  params = {
+    'territory': 'TW',    # Allowed: HK, JP, MY, SG, TW
+    'offset': 0,
+    'limit': 30
+  }
+
+  response = requests.get(url, headers=headers, params=params)
+  result = response.json()["data"]
+  for item in result:
+    print(item['id'], item['title'])
+    print('==============================')
+
+
+# https://docs-en.kkbox.codes/#get-/charts/{playlist_id}/tracks
+def get_tracks_of_chart_playlist(playlist_id):
+
+  access_token = get_access_token()
+
+  url = f'https://api.kkbox.com/v1.1/charts/{playlist_id}/tracks'
+
+  headers = {
+    'accept': 'application/json',
+    'authorization': f'Bearer {access_token}'
+  }
+
+  params = {
+    'territory': 'TW',    # Allowed: HK, JP, MY, SG, TW
+    'offset': 0,
+    'limit': 30
+  }
+
+  response = requests.get(url, headers=headers, params=params)
+  result = response.json()["data"]
+  for item in result:
+    # print(item)
+    print(get_cleaner_name(item['album']['artist']['name']))
+    print(get_cleaner_name(item['name']))
+    print('==============================')
+
+
+def get_cleaner_name(name):
+
+  cleaner_name = name
+
+  # turn
+  # "想見你想見你想見你 (Miss You 3000) - 電視劇<想見你>片尾曲"
+  # to
+  # "想見你想見你想見你 (Miss You 3000)"
+  cleaner_name = cleaner_name.split(' -')[0]
+
+  # turn
+  # "想見你想見你想見你 (Miss You 3000)"
+  # to
+  # "想見你想見你想見你"
+  cleaner_name = cleaner_name.split(' (')[0]
+
+  return cleaner_name
+
+
+# get_chart_playlists()
+get_tracks_of_chart_playlist('HZsJnXizzAH64_DrMp')
