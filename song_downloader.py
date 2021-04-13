bd45c8d7bf197c6009ec7cded88cabd8f7fda860
kanido386 <kanido386@gmail.com>
2021-04-13 13:07:43 +0800

    Song downloader: normalize downloaded audio files

--- song_downloader.py
+++ song_downloader.py
@@ -1,5 +1,13 @@
 from worker.youtube_agent import YoutubeAgent
 import webbrowser
+import os
+import librosa
+from scipy.io.wavfile import write
+import numpy as np
+
+# Turn off the warning "PySoundFile failed. Trying audioread instead."
+import warnings
+warnings.filterwarnings('ignore')
 
 class SongDownloader(object):
 
@@ -22,9 +30,24 @@ class SongDownloader(object):
       newUrl = f'{temp[0]}youtubeto{temp[1]}'
       webbrowser.open(newUrl)
 
-      # print(f'{artist} - {song}: {videoUrl}')
-      # pytube = PytubeAgent(artist, song, videoUrl)
-      # pytube.run()
+    # TODO: 比較 normalize 前後的音質差別
+    do_normalize = input('要標準化音檔的音量大小嗎(y/n)？ ')
+    if do_normalize == 'y':
+      download_folder = '../../Downloads'
+      mp3_files = [item for item in os.listdir(download_folder) if '.mp3' in item]
+      print(mp3_files)
+      for item in mp3_files:
+        current_file = f'{download_folder}/{item}'
+        # TODO: 會不會有些下載的音檔是 mono？
+        y, sr = librosa.load(current_file, mono=False)
+        left, right = y
+        left = librosa.util.normalize(left)
+        right = librosa.util.normalize(right)
+        # https://stackoverflow.com/questions/3637350/how-to-write-stereo-wav-files-in-python
+        y = np.vstack((left, right))
+        write(current_file, sr, y.T)
+    else:
+      print('fine.')
 
 
   def get_tracks_from_txt(self):
